431db300a45e83ab067f6cbf8cf7fa847a1e654b
Yotau Hong <crazymousethief@outlook.com>
2018-09-09 17:00:43 +0800

    ✨ Add union-find assignment (grade 92%)

---
+++ src/main/java/1_Fundamentals/1_5/Assignment/Percolation.java
@@ -0,0 +1,64 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    private final int n;
+    private int openCount = 0;
+    private boolean[][] grid;
+    private final WeightedQuickUnionUF uf;
+    private final int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+    public Percolation(int n) {
+        if (n <= 0) throw new IllegalArgumentException("n is expected to be greater than 0");
+        this.n = n;
+        grid = new boolean[n][n];
+        uf = new WeightedQuickUnionUF(n * n + 2); // Add virtual-top and virtual-bottom
+        for (int i = 1; i <= n; i++) {
+            uf.union(0, i);
+            uf.union(n * n + 1 - i, n * n + 1);
+        }
+    }
+
+    private boolean rangeCheck(int row, int col) {
+        return row >= 1 && row <= n && col >= 1 && col <= n;
+    }
+
+    private void rangeCheckWithException(int row, int col) {
+        if (!rangeCheck(row, col))
+            throw new IllegalArgumentException("Row or col is outside its prescribed range");
+    }
+
+    private int getIndex(int row, int col) {
+        return (row - 1) * n + col;
+    }
+
+    public void open(int row, int col) {
+        rangeCheckWithException(row, col);
+        int index = getIndex(row, col);
+        if (!grid[row - 1][col - 1]) openCount++;
+        grid[row - 1][col - 1] = true;
+        for (int[] d : dir) {
+            int nRow = row + d[0];
+            int nCol = col + d[1];
+            if (rangeCheck(nRow, nCol) && isOpen(nRow, nCol))
+                uf.union(index, getIndex(nRow, nCol));
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        rangeCheckWithException(row, col);
+        return grid[row - 1][col - 1];
+    }
+
+    public boolean isFull(int row, int col) {
+        rangeCheckWithException(row, col);
+        return isOpen(row, col) && uf.connected(0, getIndex(row, col));
+    }
+
+    public int numberOfOpenSites() {
+        return openCount;
+    }
+
+    public boolean percolates() {
+        return uf.connected(0, n * n + 1);
+    }
+}
---
+++ src/main/java/1_Fundamentals/1_5/Assignment/PercolationStats.java
@@ -0,0 +1,76 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+    private final int t;
+    private final double[] xn;
+    private double mean;
+    private double stddev;
+    private double confidenceLo;
+    private double confidenceHi;
+
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0)
+            throw new IllegalArgumentException("n or trials is expected to be grater than 0.");
+        t = trials;
+        xn = new double[trials];
+        for (int i = 0; i < trials; i++) {
+            Percolation p = new Percolation(n);
+            for (int j = 0; j < n * n && !p.percolates(); j++) {
+                int index, row, col;
+                do {
+                    index = StdRandom.uniform(0, n * n);
+                    row = index / n + 1;
+                    col = index % n + 1;
+                } while (p.isOpen(row, col));
+                p.open(row, col);
+            }
+            xn[i] = (double) p.numberOfOpenSites() / (n * n);
+        }
+    }
+
+    public double mean() {
+        if (mean > 0) {
+            return mean;
+        } else {
+            mean = StdStats.mean(xn);
+            return mean;
+        }
+    }
+
+    public double stddev() {
+        if (stddev > 0) {
+            return stddev;
+        } else {
+            stddev = StdStats.stddev(xn);
+            return stddev;
+        }
+    }
+
+    public double confidenceLo() {
+        if (confidenceLo > 0) {
+            return confidenceLo;
+        } else {
+            confidenceLo = mean() - (1.96 * stddev() / Math.sqrt(t));
+            return confidenceLo;
+        }
+    }
+
+
+    public double confidenceHi() {
+        if (confidenceHi > 0) {
+            return confidenceHi;
+        } else {
+            confidenceHi = mean() + (1.96 * stddev() / Math.sqrt(t));
+            return confidenceHi;
+        }
+    }
+
+    public static void main(String[] args) {
+        PercolationStats p = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        StdOut.printf("%-23s = %s\n", "mean", p.mean());
+        StdOut.printf("%-23s = %s\n", "stddev", p.stddev());
+        StdOut.printf("%-23s = [%s, %s]\n", "95% confidence interval", p.confidenceLo(), p.confidenceHi());
+    }
+}
