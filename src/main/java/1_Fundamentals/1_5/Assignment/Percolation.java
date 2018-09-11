8d17687b7984db97fa09c6597afe6d9ab1dc87fd
Yotau Hong <crazymousethief@outlook.com>
2018-09-11 20:30:48 +0800

    ✨ Update union-find assignment (grade 100%)

--- src/main/java/1_Fundamentals/1_5/Assignment/Percolation.java
+++ src/main/java/1_Fundamentals/1_5/Assignment/Percolation.java
@@ -4,7 +4,7 @@ public class Percolation {
     private final int n;
     private int openCount = 0;
     private boolean[][] grid;
-    private final WeightedQuickUnionUF uf;
+    private final WeightedQuickUnionUF uf, full;
     private final int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
     public Percolation(int n) {
@@ -12,8 +12,10 @@ public class Percolation {
         this.n = n;
         grid = new boolean[n][n];
         uf = new WeightedQuickUnionUF(n * n + 2); // Add virtual-top and virtual-bottom
+        full = new WeightedQuickUnionUF(n * n + 2); // Add virtual-top and virtual-bottom
         for (int i = 1; i <= n; i++) {
             uf.union(0, i);
+            full.union(0, i);
             uf.union(n * n + 1 - i, n * n + 1);
         }
     }
@@ -34,14 +36,18 @@ public class Percolation {
     public void open(int row, int col) {
         rangeCheckWithException(row, col);
         int index = getIndex(row, col);
-        if (!grid[row - 1][col - 1]) openCount++;
-        grid[row - 1][col - 1] = true;
-        for (int[] d : dir) {
-            int nRow = row + d[0];
-            int nCol = col + d[1];
-            if (rangeCheck(nRow, nCol) && isOpen(nRow, nCol))
-                uf.union(index, getIndex(nRow, nCol));
+        if (!grid[row - 1][col - 1]) {
+            openCount++;
+            for (int[] d : dir) {
+                int nRow = row + d[0];
+                int nCol = col + d[1];
+                if (rangeCheck(nRow, nCol) && isOpen(nRow, nCol)) {
+                    uf.union(index, getIndex(nRow, nCol));
+                    full.union(index, getIndex(nRow, nCol));
+                }
+            }
         }
+        grid[row - 1][col - 1] = true;
     }
 
     public boolean isOpen(int row, int col) {
@@ -51,7 +57,7 @@ public class Percolation {
 
     public boolean isFull(int row, int col) {
         rangeCheckWithException(row, col);
-        return isOpen(row, col) && uf.connected(0, getIndex(row, col));
+        return isOpen(row, col) && full.connected(0, getIndex(row, col));
     }
 
     public int numberOfOpenSites() {
@@ -59,6 +65,6 @@ public class Percolation {
     }
 
     public boolean percolates() {
-        return uf.connected(0, n * n + 1);
+        return n > 1 ? uf.connected(0, n * n + 1) : isOpen(1, 1);
     }
 }
