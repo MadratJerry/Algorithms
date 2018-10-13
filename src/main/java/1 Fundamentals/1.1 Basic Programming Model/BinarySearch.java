0e4df3836f3678aad6a17aff3ddc269e8658b274
Yotau Hong <crazymousethief@outlook.com>
2018-10-13 21:54:03 +0800

    ✨ Add BinarySearch and RandomSeq

---
+++ src/main/java/1 Fundamentals/1.1 Basic Programming Model/BinarySearch.java
@@ -0,0 +1,32 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+
+    public static int indexOf(int[] a, int key) {
+        int l = 0, r = a.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (a[mid] < key) r = mid - 1;
+            else if (a[mid] > key) l = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        int[] whiteList = in.readAllInts();
+
+        Arrays.sort(whiteList);
+
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (BinarySearch.indexOf(whiteList, key) == -1)
+                StdOut.println(key);
+        }
+    }
+}
