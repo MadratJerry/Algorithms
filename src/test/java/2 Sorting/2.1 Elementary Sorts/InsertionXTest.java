e1d46f070e5dbb7181f71887ff36ad8900597d03
Yotau Hong <crazymousethief@outlook.com>
2018-10-14 01:34:38 +0800

    🐛 Fix InsertionXTest bug

--- src/test/java/2 Sorting/2.1 Elementary Sorts/InsertionXTest.java
+++ src/test/java/2 Sorting/2.1 Elementary Sorts/InsertionXTest.java
@@ -1,9 +1,6 @@
-import edu.princeton.cs.algs4.InsertionX;
 import edu.princeton.cs.algs4.StdRandom;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.RepeatedTest;
-
-import java.util.Arrays;
 
 class InsertionXTest {
 
@@ -15,7 +12,7 @@ class InsertionXTest {
         for (int i = 0; i < size; i++) a[i] = StdRandom.uniform(size);
         System.arraycopy(a, 0, b, 0, size);
         InsertionX.sort(a);
-        Arrays.sort(b);
+        edu.princeton.cs.algs4.InsertionX.sort(b);
         Assertions.assertArrayEquals(a, b);
     }
 }
