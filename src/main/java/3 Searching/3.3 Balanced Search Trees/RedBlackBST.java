40b42c232ce107e5540c7005eaf42be4a5ee0fce
Yotau Hong <crazymousethief@outlook.com>
2018-10-29 23:39:11 +0800

    ✨ Add an unfinished RedBlackBST

---
+++ src/main/java/3 Searching/3.3 Balanced Search Trees/RedBlackBST.java
@@ -0,0 +1,176 @@
+public class RedBlackBST<Key extends Comparable<Key>, Value> implements OrderedSymbolTable<Key, Value> {
+    private final static boolean BLACK = false;
+    private final static boolean RED   = false;
+
+    private Node root;
+
+    private class Node {
+        private Key   key;
+        private Value value;
+        int  size;
+        Node left, right;
+        boolean color;
+
+        Node(Key key, Value value, int size, boolean color) {
+            this.key = key;
+            this.value = value;
+            this.size = size;
+            this.color = color;
+        }
+    }
+
+    private void checkKey(Key key) {
+        if (key == null) throw new IllegalArgumentException("The argument key is null");
+    }
+
+    private boolean isRed(Node node) {
+        return node != null && node.color == RED;
+    }
+
+    @Override
+    public void put(Key key, Value value) {
+        checkKey(key);
+
+        root = put(root, key, value);
+        root.color = BLACK;
+    }
+
+    private Node put(Node node, Key key, Value value) {
+        if (node == null) return new Node(key, value, 1, RED);
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) node.left = put(node.left, key, value);
+        else if (cmp > 0) node.right = put(node.right, key, value);
+        else node.value = value;
+
+        if (!isRed(node.left) && isRed(node.right)) node = rotateLeft(node);
+        if (isRed(node.left) && isRed(node.left.left)) node = rotateRight(node);
+        if (isRed(node.left) && isRed(node.right)) flipColors(node);
+
+        node.size = size(node.left) + size(node.right) + 1;
+        return node;
+    }
+
+    private Node rotateLeft(Node node) {
+        Node rightNode = node.right;
+        node.right = rightNode.left;
+        rightNode.left = node;
+        rightNode.color = node.color;
+        node.color = RED;
+        rightNode.size = node.size;
+        node.size = 1 + size(node.left) + size(node.right);
+        return rightNode;
+    }
+
+    private Node rotateRight(Node node) {
+        Node leftNode = node.left;
+        node.left = leftNode.right;
+        leftNode.right = node;
+        leftNode.color = node.color;
+        node.color = RED;
+        leftNode.size = node.size;
+        node.size = 1 + size(node.left) + size(node.right);
+        return leftNode;
+    }
+
+    private void flipColors(Node node) {
+        node.left.color = node.right.color = BLACK;
+        node.color = RED;
+    }
+
+    @Override
+    public Value get(Key key) {
+        checkKey(key);
+
+        return get(root, key);
+    }
+
+    private Value get(Node node, Key key) {
+        while (node != null) {
+            int cmp = key.compareTo(node.key);
+            if (cmp < 0) node = node.left;
+            else if (cmp > 0) node = node.right;
+            else return node.value;
+        }
+        return null;
+    }
+
+    @Override
+    public void delete(Key key) {
+
+    }
+
+    @Override
+    public boolean contains(Key key) {
+        return get(key) != null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    @Override
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node node) {
+        return node == null ? 0 : node.size;
+    }
+
+    @Override
+    public Iterable<Key> keys() {
+        return null;
+    }
+
+    @Override
+    public Key min() {
+        return null;
+    }
+
+    @Override
+    public Key max() {
+        return null;
+    }
+
+    @Override
+    public Key floor(Key key) {
+        return null;
+    }
+
+    @Override
+    public Key ceiling(Key key) {
+        return null;
+    }
+
+    @Override
+    public int rank(Key key) {
+        return 0;
+    }
+
+    @Override
+    public Key select(int index) {
+        return null;
+    }
+
+    @Override
+    public void deleteMin() {
+
+    }
+
+    @Override
+    public void deleteMax() {
+
+    }
+
+    @Override
+    public int size(Key l, Key r) {
+        return 0;
+    }
+
+    @Override
+    public Iterable<Key> keys(Key l, Key r) {
+        return null;
+    }
+}
